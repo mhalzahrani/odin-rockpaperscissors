8ae5507e17905c5322f90d30596a99d3ebf4cfd9
Mohammad <39103123+mhalzahrani@users.noreply.github.com>
2022-05-14 22:56:43 +0300

    refactor and simplified code

--- script.js
+++ script.js
@@ -2,56 +2,51 @@ let choices = ["Rock", "Paper", "Scissors"];
 let playerScore = 0;
 let computerScore = 0;
 
-
-// Create a function for Random Computer Play that generate a random choice from the choices and return it.
-
-function computerPlay() {
+function getComputerSelection() {
     computerSelection = choices[Math.floor(Math.random() * choices.length)];
     computerSelection = computerSelection.toLocaleLowerCase();
     return computerSelection;
 }
 
-// create the player selection function
-
-function playerPlay() {
+function getPlayerSelection() {
     playerSelection = prompt("Please choose: Rock, Paper, or Scissors ",);
     playerSelection = playerSelection.toLowerCase();
     return playerSelection;
 }
 
-// a function that plays a single round. It takes two parameters - the playerSelection and computerSelection - and  return the winner of the round
-function playRound(computerSelection, playerSelection) {
-
+function singlePlayRound(computerSelection, playerSelection) {
     console.log(`player selection is ${playerSelection}`);
     console.log(`computer selection is ${computerSelection}`);
     if (computerSelection === playerSelection) {
-        console.log('it is a draw');
         return 1;
     } else if
         ((computerSelection === 'rock' && (playerSelection === 'scissors')) ||
         (computerSelection === 'paper' && playerSelection === 'rock') ||
         (computerSelection === 'scissors' && playerSelection === 'paper')) {
-        console.log('computer win');
         return 0;
     } else {
-        console.log('player win')
     }
 }
 
-function game() {
-    let session;
+function playGame() {
+    let gameSession;
     for (let i = 0; i < 5; i++) {
-        session = playRound(computerPlay(), playerPlay());
-        if (session == 1) {
+        gameSession = singlePlayRound(getComputerSelection(), getPlayerSelection());
+        if (gameSession == 1) {
+            console.log('it is a draw');
             console.log(`score is: computer ${computerScore} - player ${playerScore}`)
-        } else if (session == 0) {
+        } else if (gameSession == 0) {
             ++computerScore;
+            console.log('computer win');
             console.log(`score is: computer ${computerScore} - player ${playerScore}`)
         } else {
             ++playerScore;
+            console.log('player win');
             console.log(`score is: computer ${computerScore} - player ${playerScore}`)
         }
     }
+    result = `final score is Player (${playerScore} Computer (${computerScore}))`
+    return result;
 }
 
-game();
+playGame();
